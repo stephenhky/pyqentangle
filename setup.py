0bda547cda3907e3a587dfee81e8c79eb9ea1845
stephenhky <stephenhky@yahoo.com.hk>
2016-11-02 17:40:23 -0400

    optimize the package

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 setup(name='pyqentangle',
       version="0.1",
       description="Quantum Entanglement for Python",
+      long_description="Schmidt decomposition for discrete and continuous bi-partite quantum systems",
+      classifiers=[
+          "Topic :: Scientific/Engineering :: Physics",
+          "Topic :: Scientific/Engineering :: Mathematics",
+          "License :: OSI Approved :: MIT License",
+      ],
+      keywords="quantum physics Schmidt decompostion entanglement",
       url="https://github.com/stephenhky/pyqentangle",
       author="Kwan-Yuet Ho",
       author_email="stephenhky@yahoo.com.hk",
-      license=None,
+      license='MIT',
       packages=['pyqentangle'],
       install_requires=[
-        'numpy',
+          'numpy',
       ],
+      include_package_data=True,
       zip_safe=False)
