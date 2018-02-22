e9ebc9b69d2c2ec82947b13a94210367249692bd
stephenhky <stephenhky@yahoo.com.hk>
2018-02-22 12:10:06 -0500

    releasing 0.19

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='pyqentangle',
-      version="0.18",
+      version="0.19",
       description="Quantum Entanglement for Python",
       long_description="Schmidt decomposition for discrete and continuous bi-partite quantum systems",
       classifiers=[
