1e8f28d22c8ee565002ce7397dbbe5703b3652c1
stephenhky <stephenhky@yahoo.com.hk>
2018-06-19 14:00:10 -0400

    map function adjustment in unit tests

--- test/testContinuousEntanglement.py
+++ test/testContinuousEntanglement.py
@@ -16,7 +16,7 @@ class testContinuousEntanglement(unittest2.TestCase):
     def testEntangledOscillators(self):
         fcn = lambda x1, x2: np.exp(-((0.5 * (x1 + x2)) ** 2)) * np.exp(-(x1 - x2) ** 2) * np.sqrt(2. / np.pi)
         decompositions = pyqentangle.continuous_schmidt_decomposition(fcn, -10., 10., -10., 10., keep=10)
-        eigenvalues = map(lambda item: item[0], decompositions)
+        eigenvalues = list(map(lambda item: item[0], decompositions))
         self.assertAlmostEqual(eigenvalues[0], 0.888888889)
         self.assertAlmostEqual(eigenvalues[1], 0.098765432)
 
