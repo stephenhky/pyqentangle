71b425d61ed6d2722fd6e5cd1f7265a337f9563f
stephenhky <stephenhky@yahoo.com.hk>
2018-06-19 22:44:02 -0400

    updated matrix multiplication

--- pyqentangle/schmidt.py
+++ pyqentangle/schmidt.py
@@ -75,7 +75,7 @@ def schmidt_decomposition(bipartitepurestate_tensor):
     rho1 = bipartitepurestate_reduceddensitymatrix(bipartitepurestate_tensor, 1)
     eigenvalues1, unitarymat1 = eig(rho1)
     inv_unitarymat1 = np.linalg.inv(unitarymat1)
-    coefmat0 = np.dot(bipartitepurestate_tensor, inv_unitarymat1)
+    coefmat0 = np.matmul(bipartitepurestate_tensor, inv_unitarymat1)
 
     decomposition = [(float(np.real(eigenvalues1[k])),
                       coefmat0[:, k] / np.sqrt(np.real(eigenvalues1[k])),
