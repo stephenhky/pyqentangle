81586adaee0fd8b911cc9c12c3bd04247943baae
stephenhky <stephenhky@yahoo.com.hk>
2018-06-20 11:12:10 -0400

    proper normalization

--- pyqentangle/schmidt.py
+++ pyqentangle/schmidt.py
@@ -78,7 +78,7 @@ def schmidt_decomposition(bipartitepurestate_tensor):
     coefmat0 = np.matmul(bipartitepurestate_tensor, inv_unitarymat1)
 
     decomposition = [(float(np.real(eigenvalues1[k])),
-                      coefmat0[:, k] / np.sqrt(np.real(eigenvalues1[k])),
+                      coefmat0[:, k] / np.linalg.norm(coefmat0[:, k]),
                       unitarymat1[:, k])
                      for k in range(mindim)]
     decomposition = sorted(decomposition, key=lambda dec: dec[0], reverse=True)
