8ed7a7d474fba768e3361a7b153b2729e11a0f83
stephenhky <stephenhky@yahoo.com.hk>
2018-07-20 13:06:56 -0400

    test case for full density matrix

---
+++ test/test_fullredden.py
@@ -0,0 +1,27 @@
+
+import unittest2
+
+import numpy as np
+
+import pyqentangle
+
+
+class TestFullDen(unittest2.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_uneventwolevels(self):
+        tensor = np.array([[0., np.sqrt(np.reciprocal(3.))],
+                           [-np.sqrt(2./3.)*1j, 0.]])
+        fulldenmat = pyqentangle.schmidt.bipartitepurestate_densitymatrix(tensor)
+
+        self.assertAlmostEqual(fulldenmat[0, 1, 0, 1], np.reciprocal(3.))
+        self.assertAlmostEqual(fulldenmat[1, 0, 1, 0], 2./3.)
+        self.assertAlmostEqual(fulldenmat[0, 1, 1, 0], np.sqrt(2)/3*1j)
+        self.assertAlmostEqual(fulldenmat[1, 0, 0, 1], -np.sqrt(2) / 3 * 1j)
+
+if __name__ == '__main__':
+    unittest2.main()
