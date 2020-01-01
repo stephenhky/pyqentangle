f8461dc8174d91ad8902c3251edac39776ab3b99
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2020-01-01 17:44:17 -0500

    floating point error in unit test fixed

--- test/testConcurrence.py
+++ test/testConcurrence.py
@@ -7,7 +7,7 @@ import pyqentangle
 class testConcurrence(unittest2.TestCase):
     def testSinglet(self):
         singlet = np.array([[0., 1.], [1.j, 0.]]) / np.sqrt(2)
-        self.assertEqual(pyqentangle.concurrence(singlet), 1.)
+        self.assertAlmostEqual(pyqentangle.concurrence(singlet), 1., delta=1e-6)
 
 
 if __name__ == '__main__':
