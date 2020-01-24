be21807656349c02cca53973dce965dba9b5fa89
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2020-01-23 21:39:35 -0500

    versioning

--- pyqentangle/__init__.py
+++ pyqentangle/__init__.py
@@ -4,4 +4,3 @@ from .schmidt import schmidt_decomposition
 from .continuous import continuous_schmidt_decomposition
 from .metrics import entanglement_entropy, participation_ratio, negativity, concurrence
 
-__version__ = '3.0.1'
