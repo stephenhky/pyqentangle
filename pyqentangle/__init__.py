2f8287a20703d97897dc874208811aa5c9b1092a
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2020-01-23 19:46:45 -0500

    reordering exceptions

--- pyqentangle/__init__.py
+++ pyqentangle/__init__.py
@@ -1,6 +1,6 @@
 
+from .utils import OutOfRangeException, UnequalLengthException, InvalidQuantumStateException
 from .schmidt import schmidt_decomposition
 from .continuous import continuous_schmidt_decomposition
-from .utils import OutOfRangeException, UnequalLengthException, InvalidQuantumStateException
 from .metrics import entanglement_entropy, participation_ratio, negativity, concurrence
 
