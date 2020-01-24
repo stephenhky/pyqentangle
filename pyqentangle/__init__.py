4b95bb30df4f9a26267f5f8e919d3b4fa7f6e71f
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2020-01-23 19:40:35 -0500

    moving all exceptions to utils.py

--- pyqentangle/__init__.py
+++ pyqentangle/__init__.py
@@ -1,6 +1,7 @@
 
 from .schmidt import schmidt_decomposition
-from .continuous import continuous_schmidt_decomposition, OutOfRangeException, UnequalLengthException
+from .continuous import continuous_schmidt_decomposition
+from .utils import OutOfRangeException, UnequalLengthException, InvalidQuantumStateException
 from .metrics import entanglement_entropy, participation_ratio, negativity, concurrence
 
 __version__ = '3.0.1'
--- pyqentangle/utils.py
+++ pyqentangle/utils.py
@@ -2,3 +2,19 @@
 class InvalidQuantumStateException(Exception):
     def __init__(self, msg):
         self.message = msg
+
+
+class OutOfRangeException(Exception):
+    def __init__(self, value):
+        self.msg = "Out of range: " + str(value)
+
+    def __str__(self):
+        return repr(self.msg)
+
+
+class UnequalLengthException(Exception):
+    def __init__(self, array1, array2):
+        self.msg = "Unequal length: " + str(len(array1)) + " vs. " + str(len(array2))
+
+    def __str__(self):
+        return repr(self.msg)
