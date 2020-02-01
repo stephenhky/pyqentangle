4ed4a15d40db61cb759e7ea2904032a1c09cfc4d
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2020-02-01 00:33:54 -0500

    norm debugged

--- test/test_harmonics.py
+++ test/test_harmonics.py
@@ -7,14 +7,17 @@ from scipy.integrate import dblquad
 from pyqentangle.quantumstates.harmonics import disentangled_gaussian_wavefcn, coupled_excited_harmonics
 
 
+normsq = lambda x: x*np.conj(x)
+
+
 class testHarmonicsNorm(unittest2.TestCase):
     def disentangled_gaussian(self):
-        norm, err = dblquad(lambda x1, x2: disentangled_gaussian_wavefcn(x1, x2),
+        norm, err = dblquad(lambda x1, x2: normsq(disentangled_gaussian_wavefcn(x1, x2)),
                             -np.inf, np.inf, lambda x: -np.inf, lambda y: np.inf)
         self.assertAlmostEqual(norm, 1, delta=abs(err))
 
     def first_excited_state(self):
         for n in range(20):
-            norm, err = dblquad(lambda x1, x2: coupled_excited_harmonics(n)(x1, x2),
+            norm, err = dblquad(lambda x1, x2: normsq(coupled_excited_harmonics(n)(x1, x2)),
                                 -np.inf, np.inf, lambda x: -np.inf, lambda y: np.inf)
             self.assertAlmostEqual(norm, 1, delta=abs(err))
