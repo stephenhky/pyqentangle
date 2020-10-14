5702a64c3f58c4a55bd25f6a5799ab014b18446b
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2020-10-14 19:11:12 -0400

    test

--- pyqentangle/__init__.py
+++ pyqentangle/__init__.py
@@ -1,6 +1,12 @@
 
+from . import tncompute
+from . import schmidt
+from . import continuous
+from . import metrics
+from . import quantumstates
+from . import cythonmodule
+
 from .utils import OutOfRangeException, UnequalLengthException, InvalidQuantumStateException
 from .schmidt import schmidt_decomposition
 from .continuous import continuous_schmidt_decomposition
 from .metrics import entanglement_entropy, participation_ratio, negativity, concurrence
-from . import quantumstates
