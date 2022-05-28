8779aafd807c8964e1917a55b24b3013f2481cca
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-05-28 17:30:00 -0400

    release 3.1.11

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def install_requirements():
 
 
 setup(name='pyqentangle',
-      version="3.1.11a1",
+      version="3.1.11",
       description="Quantum Entanglement in Python",
       long_description=package_description(),
       long_description_content_type='text/markdown',
