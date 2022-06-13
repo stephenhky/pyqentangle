7c4ed08616655c69a8dae886470f19287c9ed55b
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-06-13 14:55:30 -0400

    testing Renyi entanglement entropies

--- pyqentangle/__init__.py
+++ pyqentangle/__init__.py
@@ -5,4 +5,4 @@ from . import cythonmodule
 from .utils import OutOfRangeException, UnequalLengthException, InvalidQuantumStateException
 from .schmidt import schmidt_decomposition
 from .continuous import continuous_schmidt_decomposition
-from .metrics import entanglement_entropy, participation_ratio, negativity, concurrence
+from .metrics import entanglement_entropy, participation_ratio, negativity, concurrence, renyi_entanglement_entropy
