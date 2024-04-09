c8bcdef98ea17cefab8c70357ff7622091d080a1
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2024-04-09 18:37:50 -0400

    interpolation without Cython

--- pyqentangle/continuous.py
+++ pyqentangle/continuous.py
@@ -3,7 +3,8 @@ from itertools import product
 
 import numpy as np
 
-from .cythonmodule.interpolate import numerical_continuous_interpolation_nocheck
+# from .cythonmodule.interpolate import numerical_continuous_interpolation_nocheck
+from .cythonmodule.interpolate import interpolate
 from . import schmidt_decomposition, OutOfRangeException, UnequalLengthException
 
 
@@ -36,7 +37,8 @@ def numerical_continuous_interpolation(xarray, yarray, x):
     if not (x >= minx and x < maxx):
         raise OutOfRangeException(x)
 
-    return numerical_continuous_interpolation_nocheck(xarray, yarray, x)
+    return interpolate(xarray, yarray, x)
+    # return numerical_continuous_interpolation_nocheck(xarray, yarray, x)
 
 
 def numerical_continuous_function(xarray, yarray):
