d8462676c4e9d5ad3b0757ae19ba6291172fce32
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2026-02-14 18:30:07 -0500

    debugging for pytest

--- test/test_fullredden.py
+++ test/test_fullredden.py
@@ -1,28 +1,19 @@
 
-import unittest
+from math import sqrt
 
 import numpy as np
 
 import pyqentangle
 import pyqentangle.tncompute
+import pytest
 
 
-class TestFullDen(unittest.TestCase):
-    def setUp(self):
-        pass
+def test_uneven_two_levels(self):
+    tensor = np.array([[0., np.sqrt(np.reciprocal(3.))],
+                       [-np.sqrt(2./3.)*1j, 0.]])
+    fulldenmat = pyqentangle.tncompute.bipartitepurestate_densitymatrix(tensor)
 
-    def tearDown(self):
-        pass
-
-    def test_uneventwolevels(self):
-        tensor = np.array([[0., np.sqrt(np.reciprocal(3.))],
-                           [-np.sqrt(2./3.)*1j, 0.]])
-        fulldenmat = pyqentangle.tncompute.bipartitepurestate_densitymatrix(tensor)
-
-        self.assertAlmostEqual(fulldenmat[0, 1, 0, 1], np.reciprocal(3.))
-        self.assertAlmostEqual(fulldenmat[1, 0, 1, 0], 2./3.)
-        self.assertAlmostEqual(fulldenmat[0, 1, 1, 0], np.sqrt(2)/3*1j)
-        self.assertAlmostEqual(fulldenmat[1, 0, 0, 1], -np.sqrt(2) / 3 * 1j)
-
-if __name__ == '__main__':
-    unittest.main()
+    assert fulldenmat[0, 1, 0, 1] == pytest.approx(np.reciprocal(3.))
+    assert fulldenmat[1, 0, 1, 0] == pytest.approx(2/3)
+    assert fulldenmat[0, 1, 1, 0] == pytest.approx(sqrt(2)/3*1j)
+    assert fulldenmat[1, 0, 0, 1] == pytest.approx(-sqrt(2) / 3 * 1j)
