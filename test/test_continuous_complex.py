cd184ca88a058b8b0b7fcd1857e6e863c00541a8
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2026-02-14 18:22:44 -0500

    updated some unit tests to pytest format

--- test/test_continuous_complex.py
+++ test/test_continuous_complex.py
@@ -1,24 +1,17 @@
 
-import unittest
-
 import numpy as np
 
 import pyqentangle
 
 
-class TestContinuousComplex(unittest.TestCase):
-    def test_imagpart(self):
-        f1 = lambda x1, x2: np.exp(-0.5 * (x1 + x2) ** 2) * np.exp(-(x1 - x2) ** 2) * np.sqrt(np.sqrt(8.) / np.pi)
-        f2 = lambda x1, x2: f1(x1, x2) * np.sqrt(0.5) * (1 + 1j)
+def test_imaginary_part(self):
+    f1 = lambda x1, x2: np.exp(-0.5 * (x1 + x2) ** 2) * np.exp(-(x1 - x2) ** 2) * np.sqrt(np.sqrt(8.) / np.pi)
+    f2 = lambda x1, x2: f1(x1, x2) * np.sqrt(0.5) * (1 + 1j)
 
-        f1_modes = pyqentangle.continuous_schmidt_decomposition(f1, -10, 10, -10, 10, keep=5)
-        f2_modes = pyqentangle.continuous_schmidt_decomposition(f2, -10, 10, -10, 10, keep=5)
+    f1_modes = pyqentangle.continuous_schmidt_decomposition(f1, -10, 10, -10, 10, keep=5)
+    f2_modes = pyqentangle.continuous_schmidt_decomposition(f2, -10, 10, -10, 10, keep=5)
 
-        for f1_mode, f2_mode in zip(f1_modes, f2_modes):
-            f1_eigval = f1_mode[0]
-            f2_eigval = f2_mode[0]
-            np.testing.assert_almost_equal(f1_eigval, f2_eigval)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    for f1_mode, f2_mode in zip(f1_modes, f2_modes):
+        f1_eigval = f1_mode[0]
+        f2_eigval = f2_mode[0]
+        np.testing.assert_almost_equal(f1_eigval, f2_eigval)
--- test/test_interpolation.py
+++ test/test_interpolation.py
@@ -1,28 +1,22 @@
 
-import unittest
-
 import numpy as np
 
 from pyqentangle.continuous import numerical_continuous_function
 
 
-class TestInterpolation(unittest.TestCase):
-    def test_real(self):
-        xarray = np.array([0., 1., 2., 3.])
-        yarray = np.array([1., 3., 5., 7.])
-        f = numerical_continuous_function(xarray, yarray)
-        ys = f(np.array([1.5, 2.5]))
-        self.assertAlmostEqual(ys[0], 4.)
-        self.assertAlmostEqual(ys[1], 6.)
-
-    def test_complex(self):
-        xarray = np.array([0., 1., 2., 3.])
-        yarray = np.array([0., 1.+1.j, 2.+2.j, 3.+3.j])
-        f = numerical_continuous_function(xarray, yarray)
-        ys = f(np.array([1.5, 2.5]))
-        self.assertAlmostEqual(ys[0], 1.5+1.5j)
-        self.assertAlmostEqual(ys[1], 2.5+2.5j)
+def test_interpolation_real(self):
+    xarray = np.array([0., 1., 2., 3.])
+    yarray = np.array([1., 3., 5., 7.])
+    f = numerical_continuous_function(xarray, yarray)
+    ys = f(np.array([1.5, 2.5]))
+    self.assertAlmostEqual(ys[0], 4.)
+    self.assertAlmostEqual(ys[1], 6.)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_interpolation_complex(self):
+    xarray = np.array([0., 1., 2., 3.])
+    yarray = np.array([0., 1.+1.j, 2.+2.j, 3.+3.j])
+    f = numerical_continuous_function(xarray, yarray)
+    ys = f(np.array([1.5, 2.5]))
+    self.assertAlmostEqual(ys[0], 1.5+1.5j)
+    self.assertAlmostEqual(ys[1], 2.5+2.5j)
