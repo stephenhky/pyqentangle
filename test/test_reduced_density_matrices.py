a22dac2c1023886eb3160ab362e7b7f97336c847
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2026-02-14 19:03:45 -0500

    debugging unit tests

--- test/test_reduced_density_matrices.py
+++ test/test_reduced_density_matrices.py
@@ -11,61 +11,61 @@ def test_two_level():
 
     # subsystem A
     reddenmat0 = pyqentangle.tncompute.bipartitepurestate_reduceddensitymatrix(tensor, 0)
-    assert np.trace(reddenmat0), pytest.approx(1.0)
-    assert reddenmat0[0, 0], pytest.approx(0.6)
-    assert reddenmat0[1, 1], pytest.approx(0.4)
-    assert reddenmat0[0, 1], pytest.approx(0.+0.j)
-    assert reddenmat0[1, 0], pytest.approx(0.+0.j)
+    assert np.trace(reddenmat0) == pytest.approx(1.0)
+    assert reddenmat0[0, 0] == pytest.approx(0.6)
+    assert reddenmat0[1, 1] == pytest.approx(0.4)
+    assert reddenmat0[0, 1] == pytest.approx(0.+0.j)
+    assert reddenmat0[1, 0] == pytest.approx(0.+0.j)
 
     # subsystem B
     reddenmat1 = pyqentangle.tncompute.bipartitepurestate_reduceddensitymatrix(tensor, 1)
-    assert np.trace(reddenmat1), pytest.approx(1.)
-    assert reddenmat1[0, 0], pytest.approx(0.4)
-    assert reddenmat1[1, 1], pytest.approx(0.6)
-    assert reddenmat1[0, 1], pytest.approx(0.+0.j)
-    assert reddenmat1[1, 0], pytest.approx(0.+0.j)
+    assert np.trace(reddenmat1) == pytest.approx(1.)
+    assert reddenmat1[0, 0] == pytest.approx(0.4)
+    assert reddenmat1[1, 1] == pytest.approx(0.6)
+    assert reddenmat1[0, 1] == pytest.approx(0.+0.j)
+    assert reddenmat1[1, 0] == pytest.approx(0.+0.j)
 
 def test_three_level():
     tensor = np.array([[np.sqrt(0.5), 0.0, 0.0], [0.0, np.sqrt(0.3)*1.j, 0.0], [0.0, 0.0, np.sqrt(0.2)]])
 
     # subsystem A
     reddenmat0 = pyqentangle.tncompute.bipartitepurestate_reduceddensitymatrix(tensor, 0)
-    assert np.trace(reddenmat0), pytest.approx(1.0)
-    assert reddenmat0[0, 0], pytest.approx(0.5)
-    assert reddenmat0[1, 1], pytest.approx(0.3)
-    assert reddenmat0[2, 2], pytest.approx(0.2)
-    assert reddenmat0[1, 0], pytest.approx(0.+0.j)
-    assert reddenmat0[1, 2], pytest.approx(0.+0.j)
-    assert reddenmat0[2, 0], pytest.approx(0.+0.j)
+    assert np.trace(reddenmat0) == pytest.approx(1.0)
+    assert reddenmat0[0, 0] == pytest.approx(0.5)
+    assert reddenmat0[1, 1] == pytest.approx(0.3)
+    assert reddenmat0[2, 2] == pytest.approx(0.2)
+    assert reddenmat0[1, 0] == pytest.approx(0.+0.j)
+    assert reddenmat0[1, 2] == pytest.approx(0.+0.j)
+    assert reddenmat0[2, 0] == pytest.approx(0.+0.j)
 
     # subsystem B
     reddenmat1 = pyqentangle.tncompute.bipartitepurestate_reduceddensitymatrix(tensor, 1)
-    assert np.trace(reddenmat1), pytest.approx(1.0)
-    assert reddenmat1[0, 0], pytest.approx(0.5)
-    assert reddenmat1[1, 1], pytest.approx(0.3)
-    assert reddenmat1[2, 2], pytest.approx(0.2)
-    assert reddenmat1[2, 1], pytest.approx(0.+0.j)
-    assert reddenmat1[1, 0], pytest.approx(0.+0.j)
-    assert reddenmat1[0, 2], pytest.approx(0.+0.j)
+    assert np.trace(reddenmat1) == pytest.approx(1.0)
+    assert reddenmat1[0, 0] == pytest.approx(0.5)
+    assert reddenmat1[1, 1] == pytest.approx(0.3)
+    assert reddenmat1[2, 2] == pytest.approx(0.2)
+    assert reddenmat1[2, 1] == pytest.approx(0.+0.j)
+    assert reddenmat1[1, 0] == pytest.approx(0.+0.j)
+    assert reddenmat1[0, 2] == pytest.approx(0.+0.j)
 
 def test_two_levels_2():
     tensor = np.array([[np.sqrt(0.5), np.sqrt(0.25) * 1.j], [0., np.sqrt(0.25)]])
 
     # subsystem A
     reddenmat0 = pyqentangle.tncompute.bipartitepurestate_reduceddensitymatrix(tensor, 0)
-    assert np.trace(reddenmat0), pytest.approx(1.0)
-    assert reddenmat0[0, 0], pytest.approx(0.75)
-    assert reddenmat0[1, 1], pytest.approx(0.25)
-    assert reddenmat0[0, 1], pytest.approx(0.+0.25j)
-    assert reddenmat0[1, 0], pytest.approx(0.-0.25j)
+    assert np.trace(reddenmat0) == pytest.approx(1.0)
+    assert reddenmat0[0, 0] == pytest.approx(0.75)
+    assert reddenmat0[1, 1] == pytest.approx(0.25)
+    assert reddenmat0[0, 1] == pytest.approx(0.+0.25j)
+    assert reddenmat0[1, 0] == pytest.approx(0.-0.25j)
 
     # subsystem B
     reddenmat1 = pyqentangle.tncompute.bipartitepurestate_reduceddensitymatrix(tensor, 1)
-    assert np.trace(reddenmat1), pytest.approx(1.)
-    assert reddenmat1[0, 0], pytest.approx(0.5)
-    assert reddenmat1[1, 1], pytest.approx(0.5)
-    assert reddenmat1[0, 1], pytest.approx(0.-0.35355339j)
-    assert reddenmat1[1, 0], pytest.approx(0.+0.35355339j)
+    assert np.trace(reddenmat1) == pytest.approx(1.)
+    assert reddenmat1[0, 0] == pytest.approx(0.5)
+    assert reddenmat1[1, 1] == pytest.approx(0.5)
+    assert reddenmat1[0, 1] == pytest.approx(0.-0.35355339j)
+    assert reddenmat1[1, 0] == pytest.approx(0.+0.35355339j)
 
 
 def test_fifteen_levels():
@@ -78,5 +78,6 @@ def test_fifteen_levels():
     tensor[11, 12] = np.sqrt(0.2)
     reddenmat0 = pyqentangle.tncompute.bipartitepurestate_reduceddensitymatrix(tensor, 0)
     reddenmat1 = pyqentangle.tncompute.bipartitepurestate_reduceddensitymatrix(tensor, 0)
-    assert np.trace(reddenmat0), pytest.approx(1.0)
-    assert np.trace(reddenmat1), pytest.approx(1.0)
+
+    assert np.trace(reddenmat0) == pytest.approx(1.0)
+    assert np.trace(reddenmat1) == pytest.approx(1.0)
