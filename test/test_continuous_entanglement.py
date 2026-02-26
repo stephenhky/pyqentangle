bf823639de7a1c8748808c0a8a92414f23c51e32
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2026-02-26 17:20:16 -0500

    added index in quad integration

--- test/test_continuous_entanglement.py
+++ test/test_continuous_entanglement.py
@@ -20,13 +20,13 @@ def test_entangled_oscillators():
             assert expected_coef(i), pytest.approx(eigenvalues[i])
 
         norm1, err1 = quad(
-            lambda x1: np.real(np.conjugate(decompositions[0][1](np.array([x1])))*decompositions[0][1](np.array([x1]))),
+            lambda x1: np.real(np.conjugate(decompositions[0][1](np.array([x1])))*decompositions[0][1](np.array([x1])))[0],
             -10,
             10
         )
         print(norm1)
         norm2, err2 = quad(
-            lambda x2: np.real(np.conjugate(decompositions[0][2](np.array([x2])))*decompositions[0][2](np.array([x2]))),
+            lambda x2: np.real(np.conjugate(decompositions[0][2](np.array([x2])))*decompositions[0][2](np.array([x2])))[0],
             -10,
             10
         )
