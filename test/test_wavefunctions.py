ba8035ca61c143efd510a6e6b7f6114131417444
stephenhky <stephenhky@yahoo.com.hk>
2026-03-02 12:52:55 -0500

    test gaussian function

--- test/test_wavefunctions.py
+++ test/test_wavefunctions.py
@@ -1,5 +1,16 @@
 
 import numpy as np
+from scipy.stats import norm
 import pytest
 
+from pyqentangle.core.wavefunctions import Analytic1DWaveFunction
+
+
+def test_gaussian_wavefunction():
+    gaussian_wavefunction = Analytic1DWaveFunction(
+        norm.pdf,
+        to_vectorize=False
+    )
+
+    assert gaussian_wavefunction(0) == pytest.approx(0.3989422804014327)
 
