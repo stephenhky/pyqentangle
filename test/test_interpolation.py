08f8cae9f885af3cb98814871ccc7f14c620c891
Kwan Yuet Stephen Ho <3810067+stephenhky@users.noreply.github.com>
2026-03-03 23:14:40 -0500

    separated interpolation tests

--- test/test_interpolation.py
+++ test/test_interpolation.py
@@ -6,7 +6,7 @@ from pyqentangle.core.interpolate import numerical_continuous_function
 from pyqentangle.core.wavefunctions import InterpolatingWaveFunction
 
 
-def test_interpolation_real():
+def test_interpolation_real_deprecated():
     xarray = np.array([0., 1., 2., 3.])
     yarray = np.array([1., 3., 5., 7.], dtype=np.complex128)
     f = numerical_continuous_function(xarray, yarray)
@@ -14,13 +14,16 @@ def test_interpolation_real():
     assert ys[0] == pytest.approx(4.)
     assert ys[1] == pytest.approx(6.)
 
+def test_interpolation_real():
+    xarray = np.array([0., 1., 2., 3.])
+    yarray = np.array([1., 3., 5., 7.], dtype=np.complex128)
     wavefunction = InterpolatingWaveFunction(xarray, yarray)
     assert wavefunction(1.5) == pytest.approx(4.)
     assert wavefunction(2.5) == pytest.approx(6.)
     assert wavefunction(np.array([1.5, 2.5])) == pytest.approx(np.array([4., 6.]))
 
 
-def test_interpolation_complex():
+def test_interpolation_complex_deprecated():
     xarray = np.array([0., 1., 2., 3.])
     yarray = np.array([0., 1.+1.j, 2.+2.j, 3.+3.j])
     f = numerical_continuous_function(xarray, yarray)
@@ -28,6 +31,9 @@ def test_interpolation_complex():
     assert ys[0] == pytest.approx(1.5+1.5j)
     assert ys[1] == pytest.approx(2.5+2.5j)
 
+def test_interpolation_complex():
+    xarray = np.array([0., 1., 2., 3.])
+    yarray = np.array([0., 1. + 1.j, 2. + 2.j, 3. + 3.j])
     wavefunction = InterpolatingWaveFunction(xarray, yarray)
     assert wavefunction(1.5) == pytest.approx(1.5+1.5j)
     assert wavefunction(2.5) == pytest.approx(2.5+2.5j)
