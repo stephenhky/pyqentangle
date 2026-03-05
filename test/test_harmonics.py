693765abfc9904623908f1c16893fe2f13f487fa
stephenhky <stephenhky@yahoo.com.hk>
2026-03-05 17:44:38 -0500

    subcriptable things

--- test/test_harmonics.py
+++ test/test_harmonics.py
@@ -10,7 +10,7 @@ normsq = lambda x: x*np.conj(x)
 
 
 def test_disentangled_gaussian():
-    norm, err = dblquad(lambda x1, x2: normsq(disentangled_gaussian_wavefcn()(np.array([x1, x2]))[0]),
+    norm, err = dblquad(lambda x1, x2: normsq(disentangled_gaussian_wavefcn()(np.array([x1, x2]))),
                         -np.inf, np.inf, lambda x: -np.inf, lambda y: np.inf)
     assert norm == pytest.approx(1, abs=abs(err))
 
@@ -18,7 +18,7 @@ def test_disentangled_gaussian():
 def test_correlated_bipartite_gaussian():
     covmatrix = np.array([[2., 0.5], [0.5, 1.]])
     wavefcn = correlated_bipartite_gaussian_wavefcn(covmatrix)
-    norm, err = dblquad(lambda x1, x2: normsq(wavefcn(np.array([x1, x2]))[0]),
+    norm, err = dblquad(lambda x1, x2: normsq(wavefcn(np.array([x1, x2]))),
                         -np.inf, np.inf, lambda x: -np.inf, lambda y: np.inf)
     assert norm == pytest.approx(1, abs=abs(err))
 
